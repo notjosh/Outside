8e88f049f8785c4db15bbbdd9f4c40ab1d7ba477
Joshua May <notjosh@gmail.com>
2021-01-02 01:03:20 +0100

    Added concurrency to Vimeo verification

--- scripts/update-json/src/vimeo/filter-valid.js
+++ scripts/update-json/src/vimeo/filter-valid.js
@@ -1,4 +1,8 @@
 import fetch from "node-fetch";
+import asyncPool from "tiny-async-pool";
+
+const INTERVAL = 0;
+const CONCURRENCY = 5;
 
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
@@ -17,13 +21,41 @@ const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
  * @returns {Promise<Video[]>}
  */
 const filter = async (array, predicate) => {
-  const results = [];
+  const results = await asyncPool(CONCURRENCY, array, async (arg) =>
+    (await predicate(arg)) == null ? null : arg
+  );
 
-  for (const item of array) {
-    results.push(await predicate(item));
+  return results.filter((value) => value != null);
+};
+
+/**
+ * @param {Video} video
+ * @returns {Promise<boolean>}
+ */
+const isValidVideo = async (video) => {
+  const url = `https://player.vimeo.com/video/${video.url}/config`;
+
+  console.log(
+    `checking: #${video.id} (${url}): ${video.author} - ${video.location}`
+  );
+
+  let ok = false;
+  let status = "<unknown>";
+
+  try {
+    const response = await fetch(url);
+    ok = response.ok;
+    status = `${response.status} ${response.statusText}`;
+  } catch (error) {
+    console.log(error);
   }
 
-  return array.filter((_v, index) => results[index]);
+  console.log(`...#${video.id} (${url}): ${ok ? "✅" : "❌"} ${status}`);
+
+  // don't spam the endpoint
+  await sleep(INTERVAL);
+
+  return ok;
 };
 
 /**
@@ -31,29 +63,7 @@ const filter = async (array, predicate) => {
  * @returns {Promise<Video[]>}
  */
 const filterValid = async (videos) => {
-  const out = await filter(videos, async (video) => {
-    const url = `https://player.vimeo.com/video/${video.url}/config`;
-
-    console.log(
-      `checking: #${video.id} (${url}): ${video.author} - ${video.location}`
-    );
-
-    let ok = false;
-
-    try {
-      const response = await fetch(url);
-      ok = response.ok;
-    } catch (error) {
-      console.log(error);
-    }
-
-    console.log(`...${ok ? "✅" : "❌"}`);
-
-    // don't spam the endpoint
-    await sleep(300);
-
-    return ok;
-  });
+  const out = await filter(videos, isValidVideo);
 
   return out;
 };
