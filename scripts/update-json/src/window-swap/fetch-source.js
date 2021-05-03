b7b4b3a99a4b46124e969ebd063d571261a3eac6
Joshua May <notjosh@gmail.com>
2021-05-03 19:19:29 +0200

    Fixed fetching, when it looks like there are multiple valid-looking options. Not sure what the difference is, so we just merge arrays and fetch anything that looks like it has potential

--- scripts/update-json/src/window-swap/fetch-source.js
+++ scripts/update-json/src/window-swap/fetch-source.js
@@ -1,18 +1,21 @@
-import cheerio from "cheerio";
-import fetch from "node-fetch-retry";
+import cheerio from 'cheerio';
+import fetch from 'node-fetch-retry';
+import * as Path from 'path';
+import { promises as fsp } from 'fs';
+import { fileURLToPath } from 'url';
 
-const ROOT = "https://www.window-swap.com";
+const ROOT = 'https://www.window-swap.com';
 
 const options = { retry: 3, pause: 1000 };
 
-const fetchSource = async () => {
+const fetchSourceNetwork = async () => {
   const response = await fetch(`${ROOT}/Window`, options);
   const body = await response.text();
 
   const $ = cheerio.load(body);
-  const script = $("script")
+  const script = $('script')
     .filter((_idx, element) => {
-      const src = $(element).attr("src");
+      const src = $(element).attr('src');
 
       if (src == null) {
         return false;
@@ -20,7 +23,7 @@ const fetchSource = async () => {
 
       return src.match(/main\.[a-fA-F0-9]{20}\.js$/) != null;
     })
-    .attr("src");
+    .attr('src');
 
   if (script == null) {
     throw new Error("Couldn't find main.js script, sry");
@@ -28,7 +31,7 @@ const fetchSource = async () => {
 
   console.log(`found main.js script: ${script}`);
 
-  const normalisedPath = script.startsWith("/") ? script : `/${script}`;
+  const normalisedPath = script.startsWith('/') ? script : `/${script}`;
 
   const sourceURL = `${ROOT}${normalisedPath}`;
 
@@ -36,5 +39,16 @@ const fetchSource = async () => {
 
   return source;
 };
+
+const fetchSourceDebug = async () => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = Path.dirname(__filename);
+
+  const path = Path.join(__dirname, '../../sample.main.js');
+  const content = await fsp.readFile(path, 'utf8');
+  return content;
+};
+
+const fetchSource = fetchSourceNetwork;
 
 export default fetchSource;
--- scripts/update-json/src/window-swap/resolve.js
+++ scripts/update-json/src/window-swap/resolve.js
@@ -1,18 +1,24 @@
-import * as acorn from "acorn";
-import estraverse from "estraverse";
+import * as acorn from 'acorn';
+import estraverse from 'estraverse';
+import _ from 'lodash';
+
+const log = console.log;
+// const log = () => {};
+
+const uniq = (list) => _.uniqBy(list, 'url');
 
 /** @typedef {import("./index").Video} Video */
 
-const findVideosInModule = (body) => {
-  const variables = body.filter((node) => node.type === "VariableDeclaration");
+const findVideoSectionsInModule = (body) => {
+  const variables = body.filter((node) => node.type === 'VariableDeclaration');
   for (const variable of variables) {
-    let videosNode;
+    let videosNodes;
     estraverse.traverse(variable, {
-      fallback: "iteration",
+      fallback: 'iteration',
       enter: function (node, parent) {
-        if (node.type == "VariableDeclaration") {
+        if (node.type == 'VariableDeclaration') {
           const arrayDeclarations = node.declarations.filter((declaration) => {
-            return declaration.init?.type === "ArrayExpression";
+            return declaration.init?.type === 'ArrayExpression';
           });
           if (arrayDeclarations.length === 0) {
             return this.skip();
@@ -20,7 +26,7 @@ const findVideosInModule = (body) => {
           const videosNodesProbably = arrayDeclarations.filter(
             (arrayDeclaration) => {
               const init = arrayDeclaration.init;
-              if (init?.type !== "ArrayExpression") {
+              if (init?.type !== 'ArrayExpression') {
                 return false;
               }
               if (init.elements.length === 0) {
@@ -30,10 +36,10 @@ const findVideosInModule = (body) => {
               const hasProperty = (properties, propertyName) => {
                 return (
                   properties.find((property) => {
-                    if (property.type !== "Property") {
+                    if (property.type !== 'Property') {
                       return false;
                     }
-                    if (property.key.type !== "Identifier") {
+                    if (property.key.type !== 'Identifier') {
                       return false;
                     }
                     return property.key.name === propertyName;
@@ -41,23 +47,27 @@ const findVideosInModule = (body) => {
                 );
               };
               for (const element of init.elements) {
-                if (element == null || element.type !== "ObjectExpression") {
+                if (element == null || element.type !== 'ObjectExpression') {
                   looksOkay = false;
                   continue;
                 }
                 const hasProperties =
-                  hasProperty(element.properties, "id") &&
-                  hasProperty(element.properties, "url") &&
-                  hasProperty(element.properties, "location") &&
-                  hasProperty(element.properties, "author");
+                  hasProperty(element.properties, 'id') &&
+                  hasProperty(element.properties, 'url') &&
+                  hasProperty(element.properties, 'location') &&
+                  hasProperty(element.properties, 'author');
                 looksOkay = looksOkay || hasProperties;
               }
               return looksOkay;
             }
           );
 
-          if (videosNodesProbably.length === 1) {
-            videosNode = videosNodesProbably[0];
+          if (videosNodesProbably.length > 0) {
+            log(
+              `found multiple (${videosNodesProbably.length}) probably nodes, skipping`
+            );
+
+            videosNodes = videosNodesProbably;
           }
         } else {
           this.skip();
@@ -65,11 +75,15 @@ const findVideosInModule = (body) => {
       },
     });
 
-    if (videosNode != null) {
-      const init = videosNode.init;
-      if (init != null && init.type === "ArrayExpression") {
-        return init;
-      }
+    if (videosNodes != null) {
+      return videosNodes
+        .map((videosNode) => {
+          const init = videosNode.init;
+          if (init != null && init.type === 'ArrayExpression') {
+            return init;
+          }
+        })
+        .filter((v) => v != null);
     }
   }
 };
@@ -77,44 +91,44 @@ const findVideosInModule = (body) => {
 const extractFrom = (source) => {
   const ast = acorn.parse(source, { ecmaVersion: 2018 });
 
-  if (ast.type !== "Program") {
-    throw new Error("root node is not a Program, no way to proceed?");
+  if (ast.type !== 'Program') {
+    throw new Error('root node is not a Program, no way to proceed?');
   }
 
   const body = ast.body;
 
   const expression = body[0];
-  if (expression?.type !== "ExpressionStatement") {
-    throw new Error("expected expression to kick off, no way to proceed?");
+  if (expression?.type !== 'ExpressionStatement') {
+    throw new Error('expected expression to kick off, no way to proceed?');
   }
 
   const call = expression.expression;
-  if (call.type !== "CallExpression" || call.arguments.length != 1) {
-    throw new Error("unexpected node to kick off, no way to proceed?");
+  if (call.type !== 'CallExpression' || call.arguments.length != 1) {
+    throw new Error('unexpected node to kick off, no way to proceed?');
   }
 
   const argument = call.arguments[0];
-  if (argument.type !== "ArrayExpression" || argument.elements.length !== 3) {
-    throw new Error("unexpected node to kick off, no way to proceed?");
+  if (argument.type !== 'ArrayExpression' || argument.elements.length !== 3) {
+    throw new Error('unexpected node to kick off, no way to proceed?');
   }
 
   const element = argument.elements[1];
-  if (element?.type != "ObjectExpression") {
-    throw new Error("unexpected node to kick off, no way to proceed?");
+  if (element?.type != 'ObjectExpression') {
+    throw new Error('unexpected node to kick off, no way to proceed?');
   }
 
   const properties = element.properties;
   const modules = properties
     .map((property) => {
-      if (property.type !== "Property") {
+      if (property.type !== 'Property') {
         return;
       }
 
       let key;
 
-      if (property.key.type === "Literal") {
+      if (property.key.type === 'Literal') {
         key = property.key.value?.toString();
-      } else if (property.key.type === "Identifier") {
+      } else if (property.key.type === 'Identifier') {
         key = property.key.name;
       }
 
@@ -128,13 +142,13 @@ const extractFrom = (source) => {
       };
     })
     .map((kv) => {
-      if (kv?.value.type !== "FunctionExpression") {
+      if (kv?.value.type !== 'FunctionExpression') {
         return;
       }
 
       const body = kv.value.body;
 
-      if (body.type !== "BlockStatement") {
+      if (body.type !== 'BlockStatement') {
         return;
       }
 
@@ -151,28 +165,45 @@ const extractFrom = (source) => {
     }
 
     const body = module.value;
-    const result = findVideosInModule(body);
+    const results = findVideoSectionsInModule(body);
 
-    if (result != null) {
-      console.log("found value via key: " + module.key);
+    if (Array.isArray(results) && results.length > 0) {
+      log(`found values (${results.length}) via key: ${module.key}`);
 
-      const start = result.start;
-      const end = result.end;
-      const js = source.substring(start, end);
-      const list = eval(js);
+      const valids = results.map((result) => {
+        const start = result.start;
+        const end = result.end;
+        const js = source.substring(start, end);
+        const list = eval(js);
 
-      // we've only found that we have _some_ valid videos, so let's filter out anything that looks actually invalid
-      const valid = list.filter(
-        (video) =>
-          typeof video.id === "number" &&
-          typeof video.url === "string" &&
-          typeof video.location === "string" &&
-          typeof video.author === "string"
-      );
+        // we've only found that we have _some_ valid videos, so let's filter out anything that looks actually invalid
+        const valid = list.filter(
+          (video) =>
+            typeof video.id === 'number' &&
+            typeof video.url === 'string' &&
+            typeof video.location === 'string' &&
+            typeof video.author === 'string'
+        );
 
-      return valid;
+        return uniq(valid);
+      });
+
+      if (valids.length === 1) {
+        log(`returning video count: ${valids[0].length}`);
+        return _.sortBy(valids[0], 'url');
+      } else if (valids.length > 1) {
+        const joined = uniq([].concat.apply([], valids));
+        log(`returning merged video count: ${joined.length}`);
+
+        return _.sortBy(joined, 'url');
+      }
+    } else {
+      log('no match found in module: ' + module.key);
     }
   }
+
+  log('⚠️ no result found!');
+  return [];
 };
 
 /**
